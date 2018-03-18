ca72a566c2a64762810b54e94f66de1555b6ceb5
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-03-18 23:17:49 +0100

    Core Concept - Template Helpers

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -5,7 +5,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.render('hello', {
     name: 'Andrei',
-    dog: 'corgi'
+    dog: 'corgi',
+    title: 'I love food'
   });
 });
 
