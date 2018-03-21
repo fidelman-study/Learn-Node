09ad8fe711353b0a24237b7478386b643c3b36cf
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-03-21 23:14:29 +0100

    Creating our Store Model

---
+++ starter-files/models/Store.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+
+const storeShema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: 'Please enter a store name!'
+    },
+    slug: String,
+    description: {
+        type: String,
+        trim: true
+    },
+    tags: [String]
+});
+
+storeShema.pre('save', function(next) {
+    if (!this.isModified('name')) {
+        next(); // skip
+        return;
+    }
+
+    this.slug = slug(this.name);
+    next();
+});
+
+module.exports = mongoose.model('Store', storeShema);
