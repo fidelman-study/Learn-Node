4413ee92dabe36fb156f415daf897db2fc5c8982
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-03-24 18:54:38 +0100

    Using Async Await

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 /* controllers */
 const storeController = require('../controllers/storeController');
+/* helpers */
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', storeController.myMiddleware, storeController.homePage);
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
