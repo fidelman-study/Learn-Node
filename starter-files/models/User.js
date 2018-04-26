66c34d721680d7c4054e3abec7e29c081f53cc75
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-04-26 22:34:45 +0200

    Creating User Accounts

---
+++ starter-files/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const Schema = mongoose.Schema;
+const md5 = require('md5');
+const validator = require('validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMangoose = require('passport-local-mongoose');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Invalid Email Address'],
+    required: 'Please suply an email address'
+  },
+  name: {
+    type: String,
+    required: 'Please suply a name',
+    trim: true
+  }
+});
+
+userSchema.plugin(passportLocalMangoose, { usernameField: 'email' });
+userSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model('User', userSchema);
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 /* controllers */
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 /* helpers */
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -24,5 +25,9 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag)); // *? makes :tag optional
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+router.post('/register', userController.validateRegister)
 
 module.exports = router;
