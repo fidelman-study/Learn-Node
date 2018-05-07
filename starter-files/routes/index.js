aecfc278fa2ca35d8252debf7d735d0361fa1ca8
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-05-07 22:02:49 +0200

    Adding a Reviews Data Model

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 /* helpers */
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -48,6 +49,8 @@ router.post('/account/reset/:token', authController.confirmPasswords, catchError
 
 router.get('/map',  storeController.mapPage);
 router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
+
+router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview));  
 
 /**
  * API
