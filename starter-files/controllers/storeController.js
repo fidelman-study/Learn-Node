f8379d79fd1e40d9a3f13ec6c265150531ac7bdb
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-05-08 19:07:23 +0200

    Advanced Aggregation

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -114,6 +114,11 @@ exports.getHearts = async (req, res) => {
     res.render('stores', { title: 'Hearted Stores', stores });
 };
 
+exports.getTopStores = async (req, res) => {
+    const stores = await Store.getTopStores();
+    res.render('topStores', { title: '⭐️ Top Stores', stores });
+};
+
 /**
  * API
  */
--- starter-files/models/Store.js
+++ starter-files/models/Store.js
@@ -70,11 +70,40 @@ storeShema.pre('save', async function(next) {
     // to make more resiliant so slug are unique
 });
 
+function autopopulate(next) {
+    this.populate('reviews');
+    next();
+}
+
+storeShema.pre('find', autopopulate);
+storeShema.pre('findOne', autopopulate);
+
 storeShema.statics.getTagsList = function() {
     return this.aggregate([
         { $unwind: '$tags' }, // inwind by the tags 
         { $group: { _id: '$tags', count: { $sum: 1 } } }, // group by tags and calculate a sum
         { $sort: { count: -1 } } //sort by cound ask
+    ]);
+};
+
+storeShema.statics.getTopStores = function() {
+    return this.aggregate([
+        // Lookup Stores and populate their reviews
+        { $lookup: { from: 'reviews', localField: '_id', foreignField: 'store', as: 'reviews' } },
+        // Filter for only items that have 2 or more reviews
+        { $match: { 'reviews.1': { $exists: true } } },
+        // Add the average reviews field
+        { $project: { // addField in new versions
+            photo: '$$ROOT.photo',
+            name: '$$ROOT.name',
+            slug: '$$ROOT.slug',
+            reviews: '$$ROOT.reviews',
+            averageRating: { $avg: '$reviews.rating' }
+        } },
+        // Sort it by our new field, highest reviews first
+        { $sort: { averageRating: -1 } },
+        // Lomit to at most 10
+        { $limit: 10 }
     ]);
 };
 
